e3538d200b6d1ee4c43e65d340ef800799bb8af9
Float <dziugas.petkevicius.it@gmail.com>
2026-01-29 22:25:40 +0200

    Fix export, add folder selection

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,11 +1,42 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
-using Android.OS;
 
 namespace WorkJournal
 {
     [Activity(ScreenOrientation = ScreenOrientation.Landscape, Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        private TaskCompletionSource<string?>? _tcsFolder;
+
+        public Task<string?> PickFolderAsync()
+        {
+            _tcsFolder = new TaskCompletionSource<string?>();
+
+            var intent = new Intent(Intent.ActionOpenDocumentTree);
+            intent.AddFlags(ActivityFlags.GrantReadUriPermission | ActivityFlags.GrantWriteUriPermission);
+
+            StartActivityForResult(intent, 1001);
+
+            return _tcsFolder.Task;
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent? data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == 1001)
+            {
+                if (resultCode == Result.Ok && data != null)
+                {
+                    var uri = data.Data;
+                    _tcsFolder?.SetResult(uri?.ToString());
+                }
+                else
+                {
+                    _tcsFolder?.SetResult(null);
+                }
+            }
+        }
     }
 }
